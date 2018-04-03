377210911f4e12ead5d8713decb0313218f4c36f
Pradeep Bhadani <pradeep.bhadani@gmail.com>
2018-04-03 23:26:29 +0100

    Add tags to EC2

--- ex2/ec2.tf
+++ ex2/ec2.tf
@@ -3,4 +3,5 @@ resource "aws_instance" "tf-example-2" {
   instance_type = "${var.instance-type}"
   subnet_id     = "${var.subnet-id}"
   key_name      = "${var.keypair}"
+  tags          = "${var.tags}"
 }
