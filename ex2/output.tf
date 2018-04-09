6341bae4189430ca0c3c9ed45e89803dabfbe637
Pradeep Bhadani <pradeep.bhadani@gmail.com>
2018-04-09 21:38:36 +0100

    Fix outputs

--- ex2/output.tf
+++ ex2/output.tf
@@ -1,11 +1,11 @@
 output "instance-id" {
-  value = "aws_instance.tf-example-2.id"
+  value = "${aws_instance.tf-example-2.id}"
 }
 
 output "instance-private-ip" {
-  value = "aws_instance.tf-example-2.private_ip"
+  value = "${aws_instance.tf-example-2.private_ip}"
 }
 
 output "instance-keypair" {
-  value = "aws_instance.tf-example-2.key_name"
+  value = "${aws_instance.tf-example-2.key_name}"
 }
